37b1f57d3c0cef01dbe4038bcba541f88331e0f4
Yair <hello@agzeri.com>
2018-07-25 09:20:15 -0500

    Initial commit

---
+++ js/app.js
@@ -0,0 +1,25 @@
+//--------------------------------------------------------
+//TASK 1: Add the text 'List of Friends' to .title element  
+//--------------------------------------------------------
+
+
+
+
+//--------------------------------------------------------
+//TASK 2: Put this array of strings as <li> elements
+//        onto the friends list 
+//
+//     BONUS: do it in reverse order.
+//--------------------------------------------------------
+
+var friendsList = ['Jimmy G.', 'Scotty M.', 'Patty L.', 'Kelly F.', 'Kitty S.']
+
+
+
+//--------------------------------------------------------
+//TASK 3: Make all of the text capitalized in the <li> elements with a 
+//        class of 'urgent'
+//     
+//        BONUS: Capitalize AND make bold the elements with
+//               a class of 'urgent' 
+//--------------------------------------------------------
